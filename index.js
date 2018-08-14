fcbe8f4fbb88eafbcfa481370aca50687ea74217
Emrio <emrioisme@gmail.com>
2018-08-14 03:03:59 +0200

    update version number

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @file   Some objects and functions
  * @author Emrio || TheEmrio
- * @version 2.0.0
+ * @version 2.1.0
  * @see {@link https://github.com/TheEmrio/emrioutils/tree/master/docs Emrioutils Docs}
  */
 
