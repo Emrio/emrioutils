afdab4d0389cc26263ef86eabddeaef94ea20e93
TheEmrio <emrio@emrio.fr>
2020-05-27 18:38:51 +0200

    better module export

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ import * as rnd from './rnd'
 import * as str from './str'
 import * as time from './time'
 
-const u = { arr, debug, Debugger, fs, math, obj, rnd, str, time }
-export default u
+export { arr, debug, Debugger, fs, math, obj, rnd, str, time }
+export default { arr, debug, Debugger, fs, math, obj, rnd, str, time }
