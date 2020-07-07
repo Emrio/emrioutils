c7eff12d30a627da0a63190e7949388359b3f904
TheEmrio <emrio@emrio.fr>
2020-07-07 13:54:31 +0200

    added writeFile and mkdir to fsPromisified

--- src/fs/fsPromisified.ts
+++ src/fs/fsPromisified.ts
@@ -3,8 +3,10 @@ import { promisify } from 'util'
 
 const access = promisify(fs.access)
 const appendFile = promisify(fs.appendFile)
+const mkdir = promisify(fs.mkdir)
 const readdir = promisify(fs.readdir)
 const readFile = promisify(fs.readFile)
 const unlink = promisify(fs.unlink)
+const writeFile = promisify(fs.writeFile)
 
-export { access, appendFile, readdir, readFile, unlink }
+export { access, appendFile, mkdir, readdir, readFile, unlink, writeFile }
