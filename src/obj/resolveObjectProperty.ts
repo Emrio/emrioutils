59ed4f383b3030ec0fedd45cca93db863541b922
TheEmrio <emrio@emrio.fr>
2020-07-29 15:08:07 +0200

    better `u.fs.resolveObjectProperty`

--- src/obj/resolveObjectProperty.ts
+++ src/obj/resolveObjectProperty.ts
@@ -2,12 +2,19 @@
  * resolveObjectProperty - Returns the reference of an object sub property
  * Also creates sub properties if they do not exist
  *
- * @param  {Object}               object  Input object
- * @param  {Array<String>|String} path    Path to the propertys
- * @return {Object}                       Input object's proprty
+ * @param  {Object}               object                    Input object
+ * @param  {Array<String>|String} path                      Path to the propertys
+ * @param  {Boolean}              [ensurePropertiesExist]   Creates properties if they do not exist
+ * @return {Object}                                         Input object's proprty
  */
-export function resolveObjectProperty (object: any, path: string[] | string): any {
+export function resolveObjectProperty (object: any, path: string[] | string, ensurePropertiesExist: boolean = false): any {
   if (typeof path === 'string') return resolveObjectProperty(object, path.split('.'))
-  if (!Object.prototype.hasOwnProperty.call(object, path[0])) return undefined // object[path[0]] = {}
-  return path.length === 1 ? object[path[0]] : resolveObjectProperty(object[path[0]], path.slice(1))
+  if (!Object.prototype.hasOwnProperty.call(object, path[0])) {
+    if (ensurePropertiesExist) {
+      object[path[0]] = {}
+    } else {
+      return undefined
+    }
+  }
+  return path.length === 1 ? object[path[0]] : resolveObjectProperty(object[path[0]], path.slice(1), ensurePropertiesExist)
 }
