2fff77e75be84cb6bb97b07c983836186f1a68fc
TheEmrio <emrio@emrio.fr>
2020-10-04 17:12:48 +0200

    str: added split()

--- src/str/index.ts
+++ src/str/index.ts
@@ -1,5 +1,6 @@
 import { capitalizeFirstLetter } from './capitalizeFirstLetter'
 import { replaceText } from './replaceText'
 import { splice } from './splice'
+import { split } from './split'
 
-export { capitalizeFirstLetter, replaceText, splice }
+export { capitalizeFirstLetter, replaceText, splice, split }
