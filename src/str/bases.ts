4d4f10b9f63952097ae85d71a83c1ac8be1ea28b
TheEmrio <emrio@emrio.fr>
2020-10-17 22:55:31 +0200

    added basic bases to u.str

---
+++ src/str/bases.ts
@@ -0,0 +1,8 @@
+export const base2 = '01'
+export const base8 = '01234567'
+export const base10 = '0123456789'
+export const base16 = base10 + 'abcdef'
+export const base26 = 'abcdefghijklmnopqrstuvwxyz'
+export const base52 = base26 + base26.toUpperCase()
+export const base62 = base26 + base26.toUpperCase() + base10
+export const base64 = base62 + '+='
--- src/str/index.ts
+++ src/str/index.ts
@@ -1,6 +1,7 @@
+import { base2, base8, base10, base16, base26, base52, base62, base64 } from './bases'
 import { capitalizeFirstLetter, capitalizeFirstLetter as cfl } from './capitalizeFirstLetter'
 import { replaceText } from './replaceText'
 import { splice } from './splice'
 import { split } from './split'
 
-export { capitalizeFirstLetter, replaceText, splice, split, cfl }
+export { base2, base8, base10, base16, base26, base52, base62, base64, capitalizeFirstLetter, cfl, replaceText, splice, split }
