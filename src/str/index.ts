c71cb62ecf3a954a46993282ded30c6738db89e0
TheEmrio <emrio@emrio.fr>
2020-10-17 22:15:12 +0200

    created shortcut for u.str.capitalizeFirstLetter

--- src/str/index.ts
+++ src/str/index.ts
@@ -1,6 +1,6 @@
-import { capitalizeFirstLetter } from './capitalizeFirstLetter'
+import { capitalizeFirstLetter, capitalizeFirstLetter as cfl } from './capitalizeFirstLetter'
 import { replaceText } from './replaceText'
 import { splice } from './splice'
 import { split } from './split'
 
-export { capitalizeFirstLetter, replaceText, splice, split }
+export { capitalizeFirstLetter, replaceText, splice, split, cfl }
