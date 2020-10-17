5498807e8c773a6e7373acb4ccff1e0d52cfddca
TheEmrio <emrio@emrio.fr>
2020-10-17 23:11:19 +0200

    added u.str.base60, a useful base for internet

--- src/str/bases.ts
+++ src/str/bases.ts
@@ -4,5 +4,6 @@ export const base10 = '0123456789'
 export const base16 = base10 + 'abcdef'
 export const base26 = 'abcdefghijklmnopqrstuvwxyz'
 export const base52 = base26 + base26.toUpperCase()
+export const base60 = 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ' + base10
 export const base62 = base26 + base26.toUpperCase() + base10
 export const base64 = base62 + '+='
--- src/str/index.ts
+++ src/str/index.ts
@@ -1,7 +1,7 @@
-import { base2, base8, base10, base16, base26, base52, base62, base64 } from './bases'
+import { base2, base8, base10, base16, base26, base52, base60, base62, base64 } from './bases'
 import { capitalizeFirstLetter, capitalizeFirstLetter as cfl } from './capitalizeFirstLetter'
 import { replaceText } from './replaceText'
 import { splice } from './splice'
 import { split } from './split'
 
-export { base2, base8, base10, base16, base26, base52, base62, base64, capitalizeFirstLetter, cfl, replaceText, splice, split }
+export { base2, base8, base10, base16, base26, base52, base60, base62, base64, capitalizeFirstLetter, cfl, replaceText, splice, split }
