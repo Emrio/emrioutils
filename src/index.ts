3ddd4e11cc4a8d6ce38836cdcfd93496d13b14a4
TheEmrio <emrio@emrio.fr>
2020-10-18 19:44:00 +0200

    reexporting functions in submodules

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,21 @@ export * from './obj'
 export * from './rnd'
 export * from './str'
 export * from './time'
+
+import * as _arr from './arr'
+import * as _fs from './fs'
+import * as _math from './math'
+import * as _misc from './misc'
+import * as _obj from './obj'
+import * as _rnd from './rnd'
+import * as _str from './str'
+import * as _time from './time'
+
+export const arr = _arr
+export const fs = _fs
+export const math = _math
+export const misc = _misc
+export const obj = _obj
+export const rnd = _rnd
+export const str = _str
+export const time = _time
