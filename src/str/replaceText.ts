04275cb965e3bb1cf671a8561a62356dce95082a
TheEmrio <emrio@emrio.fr>
2020-12-21 15:04:56 +0100

    Added support for numbers and booleans in u.str.replaceText()

--- src/str/replaceText.ts
+++ src/str/replaceText.ts
@@ -2,9 +2,12 @@
  * replaceText - Replaces '{foo}' in t by the value of the property foo of the object w.
  * By default only replaces first occurence
  */
-export function replaceText (t: string, w: { [k: string]: string }, replaceAllOccurences: boolean = false): string {
+
+type AllowedTypes = string | number | boolean
+
+export function replaceText (t: string, w: { [k: string]: AllowedTypes }, replaceAllOccurences: boolean = false): string {
   for (const k in w) {
-    t = t.replace(replaceAllOccurences ? new RegExp(`{${k}}`, 'g') : `{${k}}`, w[k])
+    t = t.replace(replaceAllOccurences ? new RegExp(`{${k}}`, 'g') : `{${k}}`, w[k].toString())
   }
   return t
 }
