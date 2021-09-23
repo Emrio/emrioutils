7e722a9f68c9c189b4ca557ff2047b2bf45cca2a
Emrio <emrio@emrio.fr>
2021-09-23 13:33:57 +0200

    u.avg: added support fo weights

--- src/avg.ts
+++ src/avg.ts
@@ -1,6 +1,18 @@
+import { sum } from './sum'
+
 /**
  * Gives the mean value of a list of numbers
  */
-export function avg (values: number[]): number {
-  return values.reduce((a, c) => a + c, 0) / values.length
+export function avg (values: number[], weights?: number[]): number {
+  if (!weights) {
+    return sum(values) / values.length
+  }
+
+  if (weights.length !== values.length) {
+    throw new Error('Weights array length must be equal to values array length')
+  }
+
+  const total = sum(weights)
+
+  return avg(values.map((v, i) => v * weights[i])) / total
 }
